6fd021a9db01a06bb8d4658c4cecc77468e93c66
shahvihang07 <shah.vihang07@gmail.com>
2026-06-05 21:32:54 +0530

    improved code

--- factorial_calc.c
+++ factorial_calc.c
@@ -1,11 +1,15 @@
 // Factorial of a number
 #include<stdio.h>
-int main() {
-    int n,v_fact = 1;
-    printf("Please a enter a number : \n");
-    scanf("%d", &n);
-    for(int i = n; i >= 1; i--) {
+long long main() {
+    long long n,v_fact = 1;
+    printf("Please a enter a number : ");
+    scanf("%lld", &n);
+    for(long long i = n; i >= 1; i--) {
         v_fact = v_fact * i;
+        // printf("%lld \n", i);
+        // printf("%lld \n", v_fact);
     }
-    printf("Factorial is : %d \n", v_fact);
+    printf("Factorial is : %lld \n", v_fact);
 }
+
+// due to integer overflow , int is unable to store large character size , thus we use long long.
