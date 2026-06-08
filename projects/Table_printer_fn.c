fb0e0a9b24722bd1d8148fbae3032d1f50c92cad
shahvihang07 <shah.vihang07@gmail.com>
2026-06-08 12:37:06 +0530

    new projects

---
+++ projects/Table_printer_fn.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+void printtable(int n);
+
+
+int main() {
+    int n;
+    printf("Enter Number : \n");
+    scanf("%d", &n);
+
+    printtable(n);
+    return 0;
+}
+
+void printtable(int n) {
+    for (int i = 1; i <= 10; i++)
+    {
+        printf("%d \n", i * n);
+    }
+    
+}
+
